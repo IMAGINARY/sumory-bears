c35da09dc301db765f2be03cce0a658cd249e418
Eric Londaits <eric.londaits@gmail.com>
2024-03-29 19:19:11 -0300

    fix: Make fetch cfg loader not return text on error responses.

--- src/js/lib/loader/cfg-reader-fetch.js
+++ src/js/lib/loader/cfg-reader-fetch.js
@@ -1,6 +1,7 @@
 function CfgReaderFetch(filename) {
-  return fetch(filename, { cache: 'no-store' })
-    .then((response) => response.text());
+  return fetch(filename, { cache: 'no-store' }).then((response) =>
+    response.ok ? response.text() : ''
+  );
 }
 
 module.exports = CfgReaderFetch;
